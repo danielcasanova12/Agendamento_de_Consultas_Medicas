18179188a1fcceae3f1749934f9ce2547e65048e
Luana Copetti <86331342+LuanaCopetti@users.noreply.github.com>
2023-09-23 11:54:18 -0300

    add pages

---
+++ Agendamento/Agenda_Web/Agenda_Web/Pages/Consulta/Index.cshtml.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace Agenda_Web.Pages.Consulta
+{
+    public class Index : PageModel
+    {
+        private readonly ILogger<Index> _logger;
+
+        public Index(ILogger<Index> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
