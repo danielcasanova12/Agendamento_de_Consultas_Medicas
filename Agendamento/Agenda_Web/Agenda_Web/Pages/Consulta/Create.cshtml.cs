7636a1af4f90aeca5edc14d20579fc8cd159773c
Luana Copetti <86331342+LuanaCopetti@users.noreply.github.com>
2023-09-23 19:48:34 -0300

    consulta create

---
+++ Agendamento/Agenda_Web/Agenda_Web/Pages/Consulta/Create.cshtml.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Agenda_Web.ApiUrl; 
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+
+namespace Agenda_Web.Pages.Consulta
+{
+    public class CreateModel : PageModel
+    {
+        private readonly HttpClient _httpClient;
+        private readonly ApiUrls _apiUrls;
+
+        public CreateModel(IHttpClientFactory httpClientFactory, ApiUrls apiUrls)
+        {
+            _httpClient = httpClientFactory.CreateClient();
+            _apiUrls = apiUrls;
+            _httpClient.BaseAddress = new Uri(apiUrls.Consulta); 
+        }
+
+        [BindProperty]
+        public ConsultaModel Consulta { get; set; }
+
+        public void OnGet()
+        {
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            var apiUrl = _apiUrls.Consulta; 
+
+            try
+            {
+                var json = JsonSerializer.Serialize(Consulta);
+                var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+                var response = await _httpClient.PostAsync(apiUrl, content);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    return RedirectToPage("/Consulta/Index"); 
+                }
+                else
+                {
+                    return StatusCode((int)response.StatusCode);
+                }
+            }
+            catch (HttpRequestException)
+            {
+                return BadRequest("Erro ao se conectar à API.");
+            }
+        }
+    }
+}
