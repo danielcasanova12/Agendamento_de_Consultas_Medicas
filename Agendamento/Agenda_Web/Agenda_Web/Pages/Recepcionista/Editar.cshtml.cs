dd2650884198a5a435859967e95d8fe81efe0ff8
Luana Copetti <86331342+LuanaCopetti@users.noreply.github.com>
2023-09-23 14:40:51 -0300

    update edit recepcionista

---
+++ Agendamento/Agenda_Web/Agenda_Web/Pages/Recepcionista/Editar.cshtml.cs
@@ -0,0 +1,94 @@
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json;
+
+namespace Agenda_Web.Pages.Recepcionista
+{
+    public class EditarModel : PageModel
+    {
+        private readonly HttpClient _httpClient;
+        private readonly ApiUrls _apiUrls;
+
+        public EditarModel(ApiUrls apiUrls, IHttpClientFactory httpClientFactory)
+        {
+            _apiUrls = apiUrls;
+            _httpClient = httpClientFactory.CreateClient();
+        }
+
+        [BindProperty]
+        public RecepcionistaEditModel Recepcionista { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var apiUrl = _apiUrls.Recepcionista + $"/{id}";
+
+            try
+            {
+                var response = await _httpClient.GetAsync(apiUrl);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var content = await response.Content.ReadAsStringAsync();
+                    Recepcionista = JsonConvert.DeserializeObject<RecepcionistaEditModel>(content);
+
+                    if (Recepcionista == null)
+                    {
+                        return NotFound();
+                    }
+
+                    return Page();
+                }
+                else if (response.StatusCode == System.Net.HttpStatusCode.NotFound)
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    return StatusCode((int)response.StatusCode);
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest("Erro ao se conectar à API: " + ex.Message);
+            }
+        }
+
+        public async Task<IActionResult> OnPostAsync(int id)
+        {
+            var apiUrl = _apiUrls.Recepcionista + $"/{id}"; 
+
+            try
+            {
+                var json = JsonConvert.SerializeObject(Recepcionista);
+                var content = new StringContent(json, System.Text.Encoding.UTF8, "application/json");
+
+                var response = await _httpClient.PutAsync(apiUrl, content);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    return RedirectToPage("./Index"); 
+                }
+                else if (response.StatusCode == System.Net.HttpStatusCode.NotFound)
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    return StatusCode((int)response.StatusCode);
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest("Erro ao se conectar à API: " + ex.Message);
+            }
+        }
+    }
+}
