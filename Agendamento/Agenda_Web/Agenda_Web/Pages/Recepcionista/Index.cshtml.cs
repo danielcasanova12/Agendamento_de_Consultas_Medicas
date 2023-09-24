4635519c17fdaadd01497cc40e5321d0059b94d1
Lissa Takahashi <99764365+lissatakahashi@users.noreply.github.com>
2023-09-24 18:54:13 -0300

    index recepcionista

--- Agendamento/Agenda_Web/Agenda_Web/Pages/Recepcionista/Index.cshtml.cs
+++ Agendamento/Agenda_Web/Agenda_Web/Pages/Recepcionista/Index.cshtml.cs
@@ -2,23 +2,33 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ClassModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 
 namespace Agenda_Web.Pages.Recepcionista
 {
     public class Index : PageModel
     {
-        private readonly ILogger<Index> _logger;
-
-        public Index(ILogger<Index> logger)
+        public List<RecepcionistaModel> Recepcionistas { get; set; }
+        public Index()
         {
-            _logger = logger;
+            
         }
 
-        public void OnGet()
+        public async Task<IActionResult> OnGetAsync()
         {
+            var httpClient = new HttpClient();
+            var url = "http://localhost:5219/api/Recepcionista";
+            var requestMessage = new HttpRequestMessage(HttpMethod.Get, url);
+            var response = await httpClient.SendAsync(requestMessage);
+            var content = await response.Content.ReadAsStringAsync();
+
+            Recepcionistas = JsonConvert.DeserializeObject<List<RecepcionistaModel>>(content)!;
+            
+            return Page();
         }
     }
 }
