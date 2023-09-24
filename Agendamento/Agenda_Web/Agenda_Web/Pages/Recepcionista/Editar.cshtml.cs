963607d61af69e5325e07be3550530f8cb87cb18
Luana Copetti <86331342+LuanaCopetti@users.noreply.github.com>
2023-09-24 06:31:58 -0300

    .

--- Agendamento/Agenda_Web/Agenda_Web/Pages/Recepcionista/Editar.cshtml.cs
+++ Agendamento/Agenda_Web/Agenda_Web/Pages/Recepcionista/Editar.cshtml.cs
@@ -12,10 +12,10 @@ namespace Agenda_Web.Pages.Recepcionista
         private readonly HttpClient _httpClient;
         private readonly ApiUrls _apiUrls;
 
-        public ClassModels.EditarModelNewStruct1
+        public EditarModel(ApiUrls apiUrls, IHttpClientFactory httpClientFactory)
         {
             _apiUrls = apiUrls;
-            _httpClient = httpClientFactory.CreateClientNewStruct;
+            _httpClient = httpClientFactory.CreateClient();
         }
 
         [BindProperty]
@@ -63,7 +63,7 @@ namespace Agenda_Web.Pages.Recepcionista
 
         public async Task<IActionResult> OnPostAsync(int id)
         {
-            var apiUrl = $"https://localhost:7018/api/Recepcionista/{id}"; 
+            var apiUrl = $"https://localhost:7018/api/Recepcionista/{id}";
 
             try
             {
@@ -74,7 +74,7 @@ namespace Agenda_Web.Pages.Recepcionista
 
                 if (response.IsSuccessStatusCode)
                 {
-                    return RedirectToPage("./Index"); 
+                    return RedirectToPage("./Index");
                 }
                 else if (response.StatusCode == System.Net.HttpStatusCode.NotFound)
                 {
@@ -90,61 +90,5 @@ namespace Agenda_Web.Pages.Recepcionista
                 return BadRequest("Erro ao se conectar à API: " + ex.Message);
             }
         }
-
-    internal struct NewStruct
-    {
-        public global::System.Object Item1;
-        public global::System.Object Item2;
-
-        public NewStruct(global::System.Object item1, global::System.Object item2)
-        {
-            Item1 = item1;
-            Item2 = item2;
-        }
-
-        public override global::System.Boolean Equals(global::System.Object obj)
-        {
-            return obj is NewStruct other &&
-                   EqualityComparer<global::System.Object>.Default.Equals(Item1, other.Item1) &&
-                   EqualityComparer<global::System.Object>.Default.Equals(Item2, other.Item2);
-        }
-
-        public override global::System.Int32 GetHashCode()
-        {
-            global::System.Int32 hashCode = -1030903623;
-            hashCode = hashCode * -1521134295 + EqualityComparer<global::System.Object>.Default.GetHashCode(Item1);
-            hashCode = hashCode * -1521134295 + EqualityComparer<global::System.Object>.Default.GetHashCode(Item2);
-            return hashCode;
-        }
-
-        public void Deconstruct(out global::System.Object item1, out global::System.Object item2)
-        {
-            item1 = Item1;
-            item2 = Item2;
-        }
-
-        public static implicit operator (global::System.Object, global::System.Object)(NewStruct value)
-        {
-            return (value.Item1, value.Item2);
-        }
-
-        public static implicit operator NewStruct((global::System.Object, global::System.Object) value)
-        {
-            return new NewStruct(value.Item1, value.Item2);
-        }
     }
-
-    internal record struct NewStruct1(ApiUrls apiUrls, IHttpClientFactory httpClientFactory)
-    {
-        public static implicit operator (ApiUrls apiUrls, IHttpClientFactory httpClientFactory)(NewStruct1 value)
-        {
-            return (value.apiUrls, value.httpClientFactory);
-        }
-
-        public static implicit operator NewStruct1((ApiUrls apiUrls, IHttpClientFactory httpClientFactory) value)
-        {
-            return new NewStruct1(value.apiUrls, value.httpClientFactory);
-        }
-    }
-}
 }
