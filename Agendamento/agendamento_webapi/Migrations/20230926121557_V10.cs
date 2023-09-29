403800197ce4d06bd05f340422352d5e831f98a8
danielcasanova12 <119464431+danielcasanova12@users.noreply.github.com>
2023-09-29 07:20:55 -0300

    finalizando projeto

---
+++ Agendamento/agendamento_webapi/Migrations/20230926121557_V10.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace agendamento_webapi.Migrations
+{
+    /// <inheritdoc />
+    public partial class V10 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
